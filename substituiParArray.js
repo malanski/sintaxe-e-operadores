ea293bb7d5111122d7ba249ef55444eeeac9da52
Ulisses Malanski <malanskiwork@gamil.com>
2022-05-27 01:26:31 -0300

    add palindrome function and pair number finder function

--- substituiParArray.js
+++ substituiParArray.js
@@ -2,11 +2,8 @@ const substituiParArray = (array) => {
 
     // Quando array for vazio, retorne um array com o numero - 1
         
-    if (array.length === 0) {
-            array[0] = -1
-            return array
-        }
-
+    if (array.length === 0) return -1
+    
     // Verificar numeros pares no array e substituir por Zero
 
     for (let i = 0; i < array.length; i++){
@@ -18,5 +15,7 @@ const substituiParArray = (array) => {
     return array 
 
 }
+
+console.log(substituiParArray([]))
 
 console.log(substituiParArray([0,1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20]))
